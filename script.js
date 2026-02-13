bfe52dae25b022d9cd841b4a9caa7461f096a22f
dxrkyluv <dxrkyluv@gmail.com>
2026-02-13 23:43:50 +0200

    sunshine

---
+++ script.js
@@ -0,0 +1,245 @@
+var canvas = document.getElementById("starfield");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+var context = canvas.getContext("2d");
+var stars = 500;
+var colorrange = [0, 60, 240];
+var starArray = [];
+
+function getRandom(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+for (var i = 0; i < stars; i++) {
+    var x = Math.random() * canvas.offsetWidth;
+    var y = Math.random() * canvas.offsetHeight;
+    var radius = Math.random() * 1.2;
+    var hue = colorrange[getRandom(0, colorrange.length - 1)];
+    var sat = getRandom(50, 100);
+    var opacity = Math.random();
+    starArray.push({ x, y, radius, hue, sat, opacity });
+}
+
+var frameNumber = 0;
+var opacity = 0;
+var secondOpacity = 0;
+var thirdOpacity = 0;
+
+var baseFrame = context.getImageData(0, 0, window.innerWidth, window.innerHeight);
+
+function drawStars() {
+    for (var i = 0; i < stars; i++) {
+        var star = starArray[i];
+
+        context.beginPath();
+        context.arc(star.x, star.y, star.radius, 0, 360);
+        context.fillStyle = "hsla(" + star.hue + ", " + star.sat + "%, 88%, " + star.opacity + ")";
+        context.fill();
+    }
+}
+
+function updateStars() {
+    for (var i = 0; i < stars; i++) {
+        if (Math.random() > 0.99) {
+            starArray[i].opacity = Math.random();
+        }
+    }
+}
+
+const button = document.getElementById("valentinesButton");
+
+button.addEventListener("click", () => {
+  if (button.textContent === "Нажми на мене! ❤") {
+    
+    const video = document.createElement('video');
+    video.src = '/public/video/video.mp4'; // Вкажіть шлях до вашого відео
+    video.controls = true; // Додає кнопки паузи/гучності
+    video.autoplay = true; // Автоматичний запуск
+    video.style.maxWidth = "50%"; // Щоб не вилазило за екран
+    
+    button.replaceWith(video);
+  }
+});
+
+function drawTextWithLineBreaks(lines, x, y, fontSize, lineHeight) {
+    lines.forEach((line, index) => {
+        context.fillText(line, x, y + index * (fontSize + lineHeight));
+    });
+}
+
+function drawText() {
+    var fontSize = Math.min(30, window.innerWidth / 24); 
+    var lineHeight = 8;
+
+    context.font = fontSize + "px Comic Sans MS";
+    context.textAlign = "center";
+
+
+    context.shadowColor = "rgba(45, 45, 255, 1)";
+    context.shadowBlur = 8;
+    context.shadowOffsetX = 0;
+    context.shadowOffsetY = 0;
+
+    if(frameNumber < 250){
+        context.fillStyle = `rgba(45, 45, 255, ${opacity})`;
+ 
+        context.fillText("Кожного дня я не можу повірити, як мені пощастило", canvas.width/2, canvas.height/2);
+        opacity = opacity + 0.01;
+    }
+   
+    if(frameNumber >= 250 && frameNumber < 500){
+        context.fillStyle = `rgba(45, 45, 255, ${opacity})`;
+        context.fillText("Кожного дня я не можу повірити, як мені пощастило", canvas.width/2, canvas.height/2);
+        opacity = opacity - 0.01;
+    }
+
+
+    if(frameNumber == 500){
+        opacity = 0;
+    }
+    if(frameNumber > 500 && frameNumber < 750){
+        context.fillStyle = `rgba(45, 45, 255, ${opacity})`;
+
+        
+        if (window.innerWidth < 600) {          
+            drawTextWithLineBreaks(["серед трильйонів зірок,", "крізь мільярди років"], canvas.width / 2, canvas.height / 2, fontSize, lineHeight);
+        } else {
+            context.fillText("серед трильйонів зірок, крізь мільярди років", canvas.width/2, canvas.height/2);
+        }
+
+        opacity = opacity + 0.01;
+    }
+    if(frameNumber >= 750 && frameNumber < 1000){
+        context.fillStyle = `rgba(45, 45, 255, ${opacity})`;
+
+        if (window.innerWidth < 600) {
+            drawTextWithLineBreaks(["серед трильйонів зірок,", "крізь мільярди років"], canvas.width / 2, canvas.height / 2, fontSize, lineHeight);
+        } else {
+            context.fillText("серед трильйонів зірок, крізь мільярди років", canvas.width/2, canvas.height/2);
+        }
+
+        opacity = opacity - 0.01;
+    }
+
+    if(frameNumber == 1000){
+        opacity = 0;
+    }
+    if(frameNumber > 1000 && frameNumber < 1250){
+        context.fillStyle = `rgba(45, 45, 255, ${opacity})`;
+    
+        context.fillText("зустріти таку прекрасну дівчину, котра зробила моє життя по особливому особливим та ніжним, ", canvas.width/2, canvas.height/2);
+        opacity = opacity + 0.01;
+    }
+    if(frameNumber >= 1250 && frameNumber < 1500){
+        context.fillStyle = `rgba(45, 45, 255, ${opacity})`;
+        context.fillText("зустріти таку прекрасну дівчину, котра зробила моє життя по особливому особливим та ніжним, ", canvas.width/2, canvas.height/2);
+        opacity = opacity - 0.01;
+    }
+
+    if(frameNumber == 1500){
+        opacity = 0;
+    }
+    if(frameNumber > 1500 && frameNumber < 1750){
+        context.fillStyle = `rgba(45, 45, 255, ${opacity})`;
+
+        context.fillText("котра покриває моє серце своєю справжньою любов'ю і робить найщасливішим у всьому Всесвіті, ", canvas.width/2, canvas.height/2);
+        opacity = opacity + 0.01;
+    }
+    if(frameNumber >= 1750 && frameNumber < 2000){
+        context.fillStyle = `rgba(45, 45, 255, ${opacity})`;
+        context.fillText("котра покриває моє серце своєю справжньою любов'ю і робить найщасливішим у всьому Всесвіті, ", canvas.width/2, canvas.height/2);
+        opacity = opacity - 0.01;
+    }
+
+    if(frameNumber == 2000){
+        opacity = 0;
+    }
+    if(frameNumber > 2000 && frameNumber < 2250){
+        context.fillStyle = `rgba(45, 45, 255, ${opacity})`;
+
+        if (window.innerWidth < 600) {
+            drawTextWithLineBreaks(["мій найцінніший скарб , якого я так сильно кохаю", "і так сильно боюсь втратити"], canvas.width / 2, canvas.height / 2, fontSize, lineHeight);
+        } else {
+            context.fillText("мій найцінніший скарб, якого я так сильно кохаю і так сильно боюсь втратити", canvas.width/2, canvas.height/2);
+        }
+
+        opacity = opacity + 0.01;
+    }
+    if(frameNumber >= 2250 && frameNumber < 2500){
+        context.fillStyle = `rgba(45, 45, 255, ${opacity})`;
+
+        if (window.innerWidth < 600) {
+            drawTextWithLineBreaks(["мій найцінніший скарб , якого я так сильно кохаю", "і так сильно боюсь втратити"], canvas.width / 2, canvas.height / 2, fontSize, lineHeight);
+        } else {
+            context.fillText("мій найцінніший скарб, якого я так сильно кохаю і так сильно боюсь втратити", canvas.width/2, canvas.height/2);
+        }
+
+        opacity = opacity - 0.01;
+    }
+
+    if(frameNumber == 2500){
+        opacity = 0;
+    }
+    if(frameNumber > 2500 && frameNumber < 99999){
+        context.fillStyle = `rgba(45, 45, 255, ${opacity})`;
+
+        if (window.innerWidth < 600) {
+            drawTextWithLineBreaks(["Я кохаю тебе серденько моє", "більше ніж увесь Всесвіт разом взятий"], canvas.width / 2, canvas.height / 2, fontSize, lineHeight);
+        } else {
+            context.fillText("Я кохаю тебе серденько моє, більше ніж увесь Всесвіт разом взятий", canvas.width/2, canvas.height/2);
+        }
+
+        opacity = opacity + 0.01;
+    }
+
+    if(frameNumber >= 2750 && frameNumber < 99999){
+        context.fillStyle = `rgba(45, 45, 255, ${secondOpacity})`;
+
+
+      
+        if (window.innerWidth < 600) {
+            drawTextWithLineBreaks(["і я чекаю не дочекаюсь, щоб провести", "вічність, розділяючи цю любов разом з тобою!"], canvas.width / 2, (canvas.height/2 + 60), fontSize, lineHeight);
+        } else {
+            context.fillText("і я чекаю не дочекаюсь, щоб провести вічність, розділяючи цю любов разом з тобою!", canvas.width/2, (canvas.height/2 + 50));
+        }
+
+        secondOpacity = secondOpacity + 0.01;
+    }
+
+    if(frameNumber >= 3000 && frameNumber < 99999){
+        context.fillStyle = `rgba(45, 45, 255, ${thirdOpacity})`;
+        context.fillText("З любов'ю твій Котик, дякую що ти в мене є, ріднесенька моя<3", canvas.width/2, (canvas.height/2 + 120));
+        thirdOpacity = thirdOpacity + 0.01;
+
+        button.style.display = "block";
+    }
+
+
+     context.shadowColor = "transparent";
+     context.shadowBlur = 0;
+     context.shadowOffsetX = 0;
+     context.shadowOffsetY = 0;
+}
+
+function draw() {
+    context.putImageData(baseFrame, 0, 0);
+
+    drawStars();
+    updateStars();
+    drawText();
+
+    if (frameNumber < 99999) {
+        frameNumber++;
+    }
+    window.requestAnimationFrame(draw);
+}
+
+window.addEventListener("resize", function () {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    baseFrame = context.getImageData(0, 0, window.innerWidth, window.innerHeight);
+});
+
+window.requestAnimationFrame(draw);
